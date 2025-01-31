aa7aa10ea8f017d36ca621bddd2aac5f5c47d0e7
Nikhil Suryavanshi <nikgithub@gmail.com>
2025-01-31 12:57:55 +0530

    Stage 0

---
+++ landingpage/script.js
@@ -0,0 +1,20 @@
+gsap.from('#nav1 h1',{
+  y:60,
+  duration:1,
+  delay:1,
+  opacity:0
+})
+
+gsap.from('#main h1',{
+  y:60,
+  duration:1,
+  opacity:0,
+  delay:1
+})
+
+gsap.from('#main h5',{
+  y:60,
+  duration:1,
+  opacity:0,
+  delay:1
+})
